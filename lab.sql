2fccc2e373d9786294c41c9f9dbc7a0979788456
Nery2004 <123770230+Nery2004@users.noreply.github.com>
2025-05-17 16:54:16 -0600

    add stored procedures "updates"

--- lab.sql
+++ lab.sql
@@ -7,7 +7,7 @@ WITH tutores_5_estrellas AS (
         r.mentoria_id AS tutor_id,
         COUNT(*) AS cantidad_5_estrellas
     FROM resenas r
-    WHERE r.calificacion = 5
+    WHERE r.calificacion = 5 
     GROUP BY r.mentoria_id
 )
 SELECT 
@@ -26,6 +26,45 @@ ORDER BY t5.cantidad_5_estrellas DESC;
 
 -- 2 procedimientos almacenados
 
+--Uno para actualización:
+CREATE OR REPLACE PROCEDURE gestionar_usuario(
+    p_id INT,
+    p_operacion VARCHAR,
+    p_nombre VARCHAR DEFAULT NULL,
+    p_email VARCHAR DEFAULT NULL,
+    p_telefono VARCHAR DEFAULT NULL
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    -- Verificar existencia del usuario
+    IF NOT EXISTS (SELECT 1 FROM usuarios WHERE id = p_id) THEN
+        RAISE EXCEPTION 'El usuario con ID % no existe.', p_id;
+    END IF;
+
+    IF p_operacion = 'actualizar' THEN
+        -- Actualización del usuario
+        UPDATE usuarios
+        SET
+            nombre = COALESCE(p_nombre, nombre),
+            email = COALESCE(p_email, email),
+            telefono = COALESCE(p_telefono, telefono),
+            actualizado_en = CURRENT_TIMESTAMP
+        WHERE id = p_id;
+    ELSE
+        RAISE EXCEPTION 'Operación no válida: %. Use "actualizar" ', p_operacion;
+    END IF;
+END;
+$$;
+
+--Pste es un ejemplo para actualizar:
+CALL gestionar_usuario(
+    p_id := 1, --<<--- cambiar al que tu queres
+    p_operacion := 'actualizar', --<<--- cambiar al que tu queres
+    p_nombre := 'Gabriel Busto', --<<--- cambiar al que tu queres
+    p_email := '21073128931e1231@email.com', --<<--- cambiar al que tu queres
+    p_telefono := '53212312'--<<--- cambiar al que tu queres
+);
 -- 4 vistas
 
 -- 2 triggers
